ad70d379a13f5477ab206f137be08ad255c4fc5b
trdp30 <trdp30@gmail.com>
2021-06-02 19:43:56 +0530

    apollo-client connected to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.less";
 import Navigation from "./navigation";
 import { toast } from "react-toastify";
+import React from "react";
+import { ApolloProvider } from "@apollo/client/react";
+import ApolloClientInstance from "./store/adapters/graphql";
 
 toast.configure({
   position: "top-right"
 });
 
 function App() {
-  return <Navigation />;
+  return (
+    <ApolloProvider client={ApolloClientInstance}>
+      <Navigation />
+    </ApolloProvider>
+  );
 }
 
 export default App;
